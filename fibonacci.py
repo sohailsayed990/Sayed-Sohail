1354cf5624a0d56e515187499c93d5a5d64a575f
Sohail <55552162+sohailsayed990@users.noreply.github.com>
2019-09-27 19:10:58 +0530

    fibonacci Program

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+num=int(input("Enter the range Number ="))
+i=0
+first_value=0
+second_value=1
+while i<num:
+	if i <=1:
+		next=i
+	else:
+		next=first_value+second_value
+		first_value=second_value
+		second_value=next
+	print(next)
+	i+=1
+		
